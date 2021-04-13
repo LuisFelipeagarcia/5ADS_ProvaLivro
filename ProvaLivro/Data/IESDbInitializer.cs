28baa6fc1efd5348393d57fb32eaaf1e904b2aea
Luis Felipe <luisfelipeagarcia@gmail.com>
2021-04-13 13:36:15 -0300

    Projeto Finalizado

---
+++ ProvaLivro/Data/IESDbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaLivro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaLivro.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Livros.Any())
+            {
+                return;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ProvaLivro/Models/Livro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaLivro.Models
+{
+    public class Livro
+    {
+        [Key]
+        public long? LivroID { get; set; }
+        [Display(Name = "Código do Livro")]
+        [Required]
+        public string CodLivro { get; set; }
+        [Display(Name = "Título")]
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Autor { get; set; }
+        [Display(Name = "Editóra")]
+        [Required]
+        public string Editora { get; set; }
+        [Display(Name = "Observação")]
+        [Required]
+        public string Observacao { get; set; }
+        [Display(Name = "Descrição")]
+        [Required]
+        public string Descricao { get; set; }
+        [Display(Name = "Quantidade Estoque")]
+        [Required]
+        public string Qtda { get; set; }
+    }
+}
